fe10f9b3d91947e6b1c08b48e9f6ed4142c402de
shruthi-thupakula <shruthitupakulakrt@gmail.com>
2020-08-02 16:25:51 +0530

    Updates

---
+++ precache-manifest.65bb596dbce36cd82c1d08b9a5fe00fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed29b391f5f373e5217c88a995ed3cc4",
+    "url": "/weather-app-pro/index.html"
+  },
+  {
+    "revision": "a336ea461baead645d0b",
+    "url": "/weather-app-pro/static/css/main.16b0d391.chunk.css"
+  },
+  {
+    "revision": "abfa1ed63b685ace7057",
+    "url": "/weather-app-pro/static/js/2.a5079610.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/weather-app-pro/static/js/2.a5079610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a336ea461baead645d0b",
+    "url": "/weather-app-pro/static/js/main.7f4f4ea6.chunk.js"
+  },
+  {
+    "revision": "33c85c884d41546a9807",
+    "url": "/weather-app-pro/static/js/runtime-main.f08a1cb9.js"
+  }
+]);
